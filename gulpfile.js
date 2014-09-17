69d1d68d12188fb08dcbfd999b85cbf84f5efdcf
Sysoev <ocokanet@gmail.com>
2014-09-17 22:36:42 +0400

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var stylus = require('gulp-stylus');
+var nib = require('nib');
+
+gulp.task('default', function() {
+    var YOUR_LOCALS = {};
+    gulp.src('./pages/*.jade')
+        .pipe(jade({
+            locals: YOUR_LOCALS
+        }))
+        .pipe(gulp.dest('./dist/'));
+    gulp.src('./pages/*.styl')
+        .pipe(stylus({use: [nib()]}))
+        .pipe(gulp.dest('./dist/css/'));
+});

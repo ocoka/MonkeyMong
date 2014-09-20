67b7fd1feaf1dfaede8466ca3b95bd2501bbc1fd
Sysoev <ocokanet@gmail.com>
2014-09-20 13:11:44 +0400

    adfasdgae44t waecf

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,8 @@ gulp.task('default', function() {
     var YOUR_LOCALS = {};
     gulp.src('./pages/*.jade')
         .pipe(jade({
-            locals: YOUR_LOCALS
+            locals: YOUR_LOCALS,
+            pretty:true
         }))
         .pipe(gulp.dest('./dist/'));
     gulp.src('./pages/*.styl')

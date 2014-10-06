f06dc4cb42d4e3afdb94521a06ca4b08850c3719
Sysoev <ocokanet@gmail.com>
2014-10-06 07:46:19 +0400

    Working server 	modified:   gulpfile.js 	modified:   package.json 	deleted:    server/app.js added	server.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,12 @@ var nib = require('nib');
 
 gulp.task('default', function() {
     var YOUR_LOCALS = {};
-    gulp.src('./pages/*.jade')
+    /*gulp.src('./pages/*.jade')
         .pipe(jade({
             locals: YOUR_LOCALS,
             pretty:true
         }))
-        .pipe(gulp.dest('./dist/'));
+        .pipe(gulp.dest('./dist/'));*/
     gulp.src('./pages/*.styl')
         .pipe(stylus({use: [nib()]}))
         .pipe(gulp.dest('./dist/css/'));
